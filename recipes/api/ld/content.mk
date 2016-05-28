8d406d552a2327eaf9a0a9d82e4197e7644b3ece
Norman Feske <norman.feske@genode-labs.com>
2016-05-29 00:54:44 +0200

    Complement ld and libc recipes
    This patch adds the content needed to actually build the libc.

--- recipes/api/ld/content.mk
+++ recipes/api/ld/content.mk
@@ -1,7 +1,28 @@
-content: include LICENSE
+content: include LICENSE lib src lib/mk/ld.mk mk
 
 include:
 	$(VERBOSE)cp -r $(GENODE_DIR)/repos/base/include $@
+	$(VERBOSE)cp -r $(GENODE_DIR)/repos/base-sel4/include/base/native_types.h $@/base/
+
+lib:
+	$(VERBOSE)mkdir -p lib/mk
+	$(VERBOSE)cp $(GENODE_DIR)/repos/base/lib/mk/ldso-startup.mk lib/mk/
+	$(VERBOSE)touch lib/mk/platform.mk
+	$(VERBOSE)touch lib/mk/base.mk
+	$(VERBOSE)touch lib/mk/config.mk
+
+lib/mk/ld.mk: lib
+	$(VERBOSE) \
+	( echo "SRC_SYMBOLS = symbols"; \
+	  echo "vpath symbols \$$(REP_DIR)" ) > $@
+
+mk:
+	$(VERBOSE)mkdir -p $@
+	$(VERBOSE)cp -r $(GENODE_DIR)/repos/base/mk/spec $@
+
+src:
+	$(VERBOSE)mkdir -p src/lib/ldso/startup
+	$(VERBOSE)cp $(GENODE_DIR)/repos/base/src/lib/ldso/startup/startup.cc src/lib/ldso/startup/
 
 LICENSE:
 	$(VERBOSE)cp -r $(GENODE_DIR)/LICENSE $@
--- recipes/src/libc/content.mk
+++ recipes/src/libc/content.mk
@@ -15,6 +15,7 @@ OS_INCLUDES := \
 	os \
 	packet_stream_rx \
 	packet_stream_tx \
+	ram_fs \
 	report_session \
 	rtc_session \
 	terminal \
@@ -32,11 +33,26 @@ src:
 	$(VERBOSE)mkdir $@
 	$(VERBOSE)cp -r $(PORT_DIR)/src/* src/
 	$(VERBOSE)cp -r $(LIBPORTS_DIR)/src/lib/libc/* src/lib/libc/
+	$(VERBOSE)mkdir -p src/lib/vfs
+	$(VERBOSE)cp $(GENODE_DIR)/repos/os/src/lib/vfs/file_system_factory.cc src/lib/vfs/
 
 lib:
 	$(VERBOSE)mkdir -p lib/mk
+	$(VERBOSE)mkdir -p lib/mk/spec/arm
+	$(VERBOSE)mkdir -p lib/mk/spec/x86_32
+	$(VERBOSE)mkdir -p lib/mk/spec/x86_64
 	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/libc-*  lib/mk
 	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/libc.mk lib/mk
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/arm/libc.mk           lib/mk/spec/arm/
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/x86_32/libc.mk        lib/mk/spec/x86_32/
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/x86_64/libc.mk        lib/mk/spec/x86_64/
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/arm/libc-gen.mk       lib/mk/spec/arm/
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/x86_32/libc-gen.mk    lib/mk/spec/x86_32/
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/x86_64/libc-gen.mk    lib/mk/spec/x86_64/
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/arm/libc-setjmp.mk    lib/mk/spec/arm/
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/x86_32/libc-setjmp.mk lib/mk/spec/x86_32/
+	$(VERBOSE)cp $(LIBPORTS_DIR)/lib/mk/spec/x86_64/libc-setjmp.mk lib/mk/spec/x86_64/
+	$(VERBOSE)cp $(GENODE_DIR)/repos/os/lib/mk/vfs.mk lib/mk
 
 LICENSE:
 	@(echo "Based on FreeBSD, which is BSD licensed:"; \
